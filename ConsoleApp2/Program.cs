3ae5fd9fc9c93481f2fcd7b2ae2d141f5ea53f80
RomaGulis <108432937+RomaGulis@users.noreply.github.com>
2022-06-29 23:51:06 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte lifes = 3;
+            bool isMooving = false;
+            string endLevel = "Congratulations!";
+            int playerDamage = 40;
+            float playerSpeed = 5f;
+            double distanceToFinish = 1000;
+            ulong numberCoins = 10000000;
+            char secretKey = '$';
+            byte scaleOfHappiness = 127;
+            long numberHouses = 476;
+            
+        }
+    }
+}
